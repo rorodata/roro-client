40417916f46eb75685464c78ae67c85371783819
sohail <sohail.bphc@gmail.com>
2017-08-24 15:52:40 +0530

    Implement roro deploy command.

--- roro/projects.py
+++ roro/projects.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import yaml
 import firefly
 
@@ -24,6 +25,24 @@ class Project:
     def logs(self, jobid):
         return self.client.logs(project=self.name, jobid=jobid)
         #return self.client.logs(project=self.name)
+
+    def deploy(self):
+        archive = self.archive()
+        size = os.path.getsize(archive)
+        with open(archive, 'rb') as f:
+            format = 'tar'
+            response =  self.client.deploy(
+                project=self.name,
+                archived_project=f,
+                size=size,
+                format=format
+            )
+        return response
+
+    def archive(self, format='tar'):
+        root_dir = os.path.realpath(os.path.curdir)
+        dir_name = os.path.basename(root_dir)
+        return shutil.make_archive(dir_name, format)
 
 def current_project():
     if os.path.exists("roro.yml"):
