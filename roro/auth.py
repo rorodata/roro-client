47df8f3fcc40e91940af5a1494edec2b821e4f00
Nabarun Pal <pal.nabarun95@gmail.com>
2017-09-27 17:12:08 +0530

    Fixes build errors by improving cross version compatibility

--- roro/auth.py
+++ roro/auth.py
@@ -64,11 +64,15 @@ class netrc(_netrc):
         # work-around to fix that
         if file is None:
             file = self.find_default_file()
-        super().__init__(file=file)
+
+        if PY2:
+            _netrc.__init__(self, file=file)
+        else:
+            super().__init__(file=file)
 
     def find_default_file(self):
         filename = "_netrc" if sys.platform == 'win32' else ".netrc"
-        p = pathlib.Path.home().joinpath(filename)
+        p = pathlib.Path(os.path.expanduser('~')).joinpath(filename)
         return str(p)
 
     def __repr__(self):
