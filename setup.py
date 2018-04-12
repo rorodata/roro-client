75b4087993be1fd0fa5d86d9dfd85ab0ff0f4c3e
Anand Chitipothu <anandology@gmail.com>
2018-04-12 12:33:02 +0530

    Moved from flit to setup.py
    Specifying git repo in requirements.txt doesn't work in that library is
    using flit.

---
+++ setup.py
@@ -0,0 +1,68 @@
+"""
+roro
+----
+
+The client interface to the rorodata platform.
+
+Install
+~~~~~~~
+
+It can be installed using pip.
+
+..code:: bash
+
+    $ pip install roro
+
+Links
+~~~~~
+
+* `Documentation <https://rorodata.com/docs/>`_
+* `Github <https://github.com/rorodata/roro-client>`_
+"""
+
+from setuptools import setup, find_packages
+import os.path
+import sys
+
+PY2 = (sys.version_info.major == 2)
+
+def get_version():
+    """Returns the package version taken from version.py.
+    """
+    root = os.path.dirname(__file__)
+    version_path = os.path.join(root, "roro/__init__.py")
+    with open(version_path) as f:
+        code = f.read()
+        env = {}
+        exec(code, env, env)
+        return env['__version__']
+
+install_requires = [
+    'firefly-python>=0.1.9',
+    'click>=6.7',
+    'tabulate>=0.7.7',
+    'PyYAML>=3.12',
+    'joblib>=0.11',
+]
+
+if PY2:
+    install_requires.append('pathlib>=1.0.1')
+    install_requires.append('backports.tempfile')
+
+__version__ = get_version()
+
+setup(
+    name='roro',
+    version=__version__,
+    author='rorodata',
+    author_email='rorodata.team@gmail.com',
+    description='The client interface to the rorodata platform',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points='''
+        [console_scripts]
+        roro = roro.cli:cli
+    ''',
+)
