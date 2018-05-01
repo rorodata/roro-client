cc82930590daafbcc461ae3162a4b1af394a79ff
Anand Chitipothu <anandology@gmail.com>
2018-05-01 17:06:17 +0530

    Made Task available from toplevel module.
    Users can use it as:
    
        from roro import Task

--- roro/__init__.py
+++ roro/__init__.py
@@ -1,7 +1,7 @@
 """roro - commandline tool for accessing all the sevices in RorodataPlatform.
 """
 
-from .projects import get_current_project
+from .projects import get_current_project, Task
 from firefly import Client
 
 __version__ = '0.1.15'
